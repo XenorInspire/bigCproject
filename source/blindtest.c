68b9724ab92fc165470c6ccbc3f01771bed6c454
Nicolas <nicolas.fouchard94370@gmail.com>
2019-11-24 16:14:32 +0100

    ADD variables from config.ini

--- source/blindtest.c
+++ source/blindtest.c
@@ -1,9 +1,28 @@
-#include "../lib/fmod/fmod.h"
+// #include "../lib/fmod/fmod.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <windows.h>
+#include <stdint.h>
 
 int main(int argc, char const *argv[]) {
-  /* code */
+
+  int16_t easy_level_solo_mode;
+  int16_t easy_level_multi_mode;
+  int16_t medium_level_solo_mode;
+  int16_t medium_level_multi_mode;
+  int16_t hard_level_solo_mode;
+  int16_t hard_level_multi_mode;
+  int16_t volume;
+
+  char * songs_directory;
+  songs_directory = malloc(256 * sizeof(char));
+
+  char * fonts_directory;
+  fonts_directory = malloc(256 * sizeof(char));
+
+  int16_t artist_score;
+  int16_t title_score;
+
   return 0;
+  
 }
