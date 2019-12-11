5145734fa815a2dedb74843863c8a06ea488c20f
Tony2464 <anthony.fargette@yahoo.fr>
2019-12-11 13:23:14 +0100

    UPDATE manage_song.h

--- source/includes/manage_song.h
+++ source/includes/manage_song.h
@@ -3,3 +3,11 @@
 int8_t copy_file(const char * source_path, const char * dest_path);
 
 int8_t delete_file(const char * source_file);
+
+long int file_size(const char * path);
+
+int8_t create_xml(const char * file_name);
+
+char * file_content(const char * file_name, int line_size);
+
+int8_t verify_xml(const char * file_name);
