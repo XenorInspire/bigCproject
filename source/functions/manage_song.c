aa8ff42d35d349a1259ddf9f81886f067710a224
Tony2464 <anthony.fargette@yahoo.fr>
2019-12-11 11:19:14 +0100

    ADD file_size, create_xml, file_content and verify_xml

--- source/functions/manage_song.c
+++ source/functions/manage_song.c
@@ -4,6 +4,7 @@
 #include <string.h>
 
 #include "../includes/verify.h"
+#include "../include/xml.h"
 
 //Copie d'un fichier vers le nom du dossier
 int8_t copy_file(const char * source_path, const char * dest_path){
@@ -52,4 +53,69 @@ int8_t delete_file(const char * source_file){
     return 1;
   }
 
+}
+
+//Donne la taille du fichier
+long int file_size(const char * path){
+  if (does_file_exist(path))
+    return -1;
+
+  FILE * fp = NULL;
+  fp = fopen(path,"r");
+
+  fseek(fp, 0, SEEK_END);
+  long int sz = ftell(fp);
+  fseek(fp, 0, SEEK_SET);
+
+  fclose(fp);
+
+  return sz;
+
+}
+
+//Recreation du fichier xml
+int8_t create_xml(const char * file_name){
+  FILE * xml_file = NULL;
+  xml_file = fopen(file_name,"wb");
+  char * root;
+  root = malloc(100 * sizeof(char));
+  if (root == NULL)
+    return -1;
+
+  strcpy(root, "<songs>\n</songs>");
+  fprintf(xml_file, "%s\n",root);
+
+  fclose(xml_file);
+  return 0;
+}
+
+//Retourne le pointeur du contenu du fichier dans un buffer
+char * file_content(const char * file_name, int line_size){
+
+  char * pointer_content = NULL;
+  pointer_content = malloc(file_size(file_name) * sizeof(char));
+  check_memory(pointer_content);
+
+  strcpy(pointer_content,"\0");
+
+  char buffer[line_size];
+  FILE * fp = NULL;
+	fp = fopen(file_name,"r");
+
+	while (fgets(buffer,line_size,fp) != NULL) {
+
+		strcat(pointer_content,buffer);
+	}
+  // printf("%s",pointer_content);
+	fclose(fp);
+
+  return pointer_content;
+}
+
+//Verif si le fichier xml existe sinon le cree, sa structure
+int8_t verify_xml(const char * file_name){
+  if (does_file_exist(file_name)) {
+    create_xml("library.xml");
+  }
+  return 0;
 }
