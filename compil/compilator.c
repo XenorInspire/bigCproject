b5658be17f14f88cfd2b4937045d1038c5e73f93
Nicolas <nicolas.fouchard94370@gmail.com>
2019-12-21 17:34:52 +0100

    ADD settings.c in compilator

--- compil/compilator.c
+++ compil/compilator.c
@@ -1,15 +1,13 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 int main(int argc, char const *argv[]) {
 
   char choice = 'o';
-  char choice_launch;
-
-  printf("Avec lancement automatique du programme ? (o/n)\n");
-  scanf("%c",&choice_launch);
-
   while(choice == 'o'){
+
+    if((argv[1] != NULL) && ((strnicmp(argv[1],"-c",2)) == 0)) system("clear");
 
     system("gcc -c ../source/functions/generate.c");
     system("gcc -c ../source/functions/init.c");
@@ -17,19 +15,17 @@ int main(int argc, char const *argv[]) {
     system("gcc -c ../source/functions/manage_song.c");
     system("gcc -c ../source/functions/xml.c");
     system("gcc -c ../source/blindtest.c");
-    system("gcc blindtest.o xml.o init.o generate.o verify.o manage_song.o -o blindtest.exe");
+    system("gcc -c ../source/functions/settings.c");
+    system("gcc blindtest.o xml.o settings.o init.o generate.o verify.o manage_song.o -o blindtest.exe");
 
     system("mv generate.o ../source/functions/generate.o");
     system("mv init.o ../source/functions/init.o");
+    system("mv settings.o ../source/functions/settings.o");
     system("mv verify.o ../source/functions/verify.o");
     system("mv manage_song.o ../source/functions/manage_song.o");
     system("mv xml.o ../source/functions/xml.o");
     system("mv blindtest.o ../source/blindtest.o");
     system("mv blindtest.exe ../source/blindtest.exe");
-
-    if(choice_launch == 'o') {
-      system("start ../source/blindtest.exe");
-    }
 
     printf("Recompiler ? (o/n) \n");
     fflush(stdin);
